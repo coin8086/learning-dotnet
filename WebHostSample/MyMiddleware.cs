2b1eb900a0fbdf2260aab870d504db43f19c7603
Robert Zhang <louirobert@gmail.com>
2021-08-26 09:09:14 +0800

    Rename project WebSample to WebHostSample.

--- WebHostSample/MyMiddleware.cs
+++ WebHostSample/MyMiddleware.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebSample
+namespace WebHostSample
 {
     public class MyMiddleware
     {
--- WebHostSample/Program.cs
+++ WebHostSample/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebSample
+namespace WebHostSample
 {
     public class Program
     {
