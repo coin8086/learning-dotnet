90625edb7638f0d6a0b3daa65a25fbda9e7e3f49
Robert Zhang <louirobert@gmail.com>
2021-08-26 09:03:02 +0800

    Show usage of Middleware and how to make a custom Middleware.

---
+++ WebSample/MyMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSample
+{
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private string _msg;
+
+        public MyMiddleware(RequestDelegate next, string msg)
+        {
+            Console.WriteLine($"MyMiddleware::MyMiddleware: {msg}");
+            _next = next;
+            _msg = msg;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"MyMiddleware::InvokeAsync {_msg} In");
+            await _next(context);
+            Console.WriteLine($"MyMiddleware::InvokeAsync {_msg} Out");
+        }
+    }
+
+    public static class MyMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder builder, string msg)
+        {
+            return builder.UseMiddleware<MyMiddleware>(msg);
+        }
+    }
+}
--- WebSample/Startup.cs
+++ WebSample/Startup.cs
@@ -18,7 +18,7 @@ namespace WebSample
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            Program.ShowConfig(configuration);
+            //Program.ShowConfig(configuration);
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -35,15 +35,31 @@ namespace WebSample
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseMyMiddleware("Hellooooo!");
 
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                Console.WriteLine("Use In");
+                await next.Invoke();
+                Console.WriteLine("Use Out");
             });
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("Run In");
+                await context.Response.WriteAsync("Hello, Middleware!");
+                Console.WriteLine("Run Out");
+            });
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
