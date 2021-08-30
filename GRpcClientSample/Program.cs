f3d84de933582615e60793b4016d366aa9548f9c
Robert Zhang <louirobert@gmail.com>
2021-08-30 16:47:27 +0800

    Show usage of streaming gRPC.

--- GRpcClientSample/Program.cs
+++ GRpcClientSample/Program.cs
@@ -1,5 +1,7 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GRpcClientSample
@@ -13,11 +15,117 @@ namespace GRpcClientSample
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting from server: " + reply.Message);
+            Console.WriteLine();
 
             var client2 = new Example.ExampleClient(channel);
-            var response = await client2.UnaryCallAsync(new ExampleRequest() { PageIndex = 1, PageSize = 3, IsDescending = false });
-            Console.WriteLine($"Request: PageIndex={response.Request.PageIndex}, PageSize={response.Request.PageSize}");
+
+            Console.WriteLine("Unary:");
+            var request = new ExampleRequest() { PageIndex = 1, PageSize = 3, IsDescending = false };
+            var response = await client2.UnaryCallAsync(request);
+            Console.WriteLine($"Request: PageIndex={request.PageIndex}, PageSize={request.PageSize}, IsDescending={request.IsDescending}");
             Console.WriteLine($"Response: {string.Join(", ", response.Titles)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Server Stream:");
+            Console.WriteLine($"Request: PageIndex={request.PageIndex}, PageSize={request.PageSize}, IsDescending={request.IsDescending}");
+            using (var source = new CancellationTokenSource())
+            {
+                using (var call = client2.StreamingFromServer(request, cancellationToken: source.Token))
+                {
+                    try
+                    {
+                        int i = 0;
+                        while (await call.ResponseStream.MoveNext(source.Token))
+                        {
+                            var response2 = call.ResponseStream.Current;
+                            Console.WriteLine($"Response: {string.Join(", ", response2.Titles)}");
+                            if (++i > 2)
+                            {
+                                //This will cancel the client request and thus MoveNext will throw an exception(from server ?).
+                                source.Cancel();
+                            }
+                        }
+                    }
+                    catch (Grpc.Core.RpcException)
+                    {
+                        Console.WriteLine("Request was cancled by client.");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Client Stream:");
+            using (var source = new CancellationTokenSource())
+            {
+                using (var call = client2.StreamingFromClient(cancellationToken: source.Token))
+                {
+                    try
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            //If request is cancled, then:
+                            //1) No response can be gotten from server(call.ResponseAsync)
+                            //2) Calls of call.RequestStream.WriteAsync and call.ResponseAsync will throw exception RpcException
+                            //if (i == 2)
+                            //{
+                            //    source.Cancel();
+                            //}
+                            Console.WriteLine($"Request: PageIndex={request.PageIndex}, PageSize={request.PageSize}, IsDescending={request.IsDescending}");
+                            await call.RequestStream.WriteAsync(request);
+                            await Task.Delay(TimeSpan.FromSeconds(1));
+                        }
+                        await call.RequestStream.CompleteAsync();
+                        var response3 = await call.ResponseAsync;
+                        Console.WriteLine($"Response: {string.Join(", ", response3.Titles)}");
+                    }
+                    catch (Grpc.Core.RpcException)
+                    {
+                        Console.WriteLine("Request was cancled by client.");
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Duplex Stream:");
+            using (var source = new CancellationTokenSource())
+            {
+                using (var call = client2.StreamingBothWays(cancellationToken: source.Token))
+                {
+                    var responser = Task.Run(async () =>
+                    {
+                        await foreach (var response4 in call.ResponseStream.ReadAllAsync())
+                        {
+                            Console.WriteLine($"Response for request: PageIndex={response4.Request.PageIndex}, PageSize={response4.Request.PageSize}, IsDescending={response4.Request.IsDescending}");
+                            Console.WriteLine($"Response: {string.Join(", ", response4.Titles)}");
+                        }
+                    });
+
+                    try
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            //If request is cancled, then:
+                            //1) No response can be gotten from server(call.ResponseAsync)
+                            //2) Calls of call.RequestStream.WriteAsync and call.ResponseAsync will throw exception RpcException
+                            //if (i == 2)
+                            //{
+                            //    source.Cancel();
+                            //}
+                            await Task.Delay(TimeSpan.FromSeconds(1));
+                            Console.WriteLine($"Request: PageIndex={request.PageIndex}, PageSize={request.PageSize}, IsDescending={request.IsDescending}");
+                            await call.RequestStream.WriteAsync(request);
+                            request.PageIndex++;
+                        }
+                        await call.RequestStream.CompleteAsync();
+                        await responser;
+                    }
+                    catch (Grpc.Core.RpcException)
+                    {
+                        Console.WriteLine("Request was cancled by client.");
+                    }
+                }
+            }
+
         }
     }
 }
--- GRpcSample/Services/ExampleService.cs
+++ GRpcSample/Services/ExampleService.cs
@@ -25,6 +25,12 @@ namespace GRpcSample
             "Dart",
         };
 
+        private IEnumerable<string> GetTitles(int pageIndex, int pageSize, bool isDescending)
+        {
+            IEnumerable<string> source = isDescending ? _titles.Reverse() : _titles;
+            return  source.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+
         public ExampleService(ILogger<ExampleService> logger)
         {
             _logger = logger;
@@ -32,26 +38,66 @@ namespace GRpcSample
 
         public override Task<ExampleResponse> UnaryCall(ExampleRequest request, ServerCallContext context)
         {
-            IEnumerable<string> source = request.IsDescending ?  _titles.Reverse() : _titles;
-            var result = source.Skip(request.PageIndex * request.PageSize).Take(request.PageSize);
             var response = new ExampleResponse() { Request = request };
-            response.Titles.Add(result);
+            response.Titles.Add(GetTitles(request.PageIndex, request.PageSize, request.IsDescending));
             return Task.FromResult(response);
         }
 
-        public override Task StreamingFromServer(ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        public override async Task StreamingFromServer(ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
         {
-            return base.StreamingFromServer(request, responseStream, context);
+            var response = new ExampleResponse() { Request = request };
+            response.Titles.Add(GetTitles(request.PageIndex, request.PageSize, request.IsDescending));
+
+            //For demo purpose, the same response is sent repeatedly until client cancels it.
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(response);
+                try
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+                }
+                catch (TaskCanceledException)
+                {
+                    _logger.LogWarning("Cancled by client.");
+                }
+            }
         }
 
-        public override Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        public override async Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
         {
-            return base.StreamingFromClient(requestStream, context);
+            var response = new ExampleResponse();
+            try
+            {
+                //When the request is cancled by client, an IOException will be thrown.
+                while (await requestStream.MoveNext(context.CancellationToken))
+                {
+                    var request = requestStream.Current;
+                    response.Titles.Add(GetTitles(request.PageIndex, request.PageSize, request.IsDescending));
+                }
+            }
+            catch (System.IO.IOException ex)
+            {
+                _logger.LogWarning(ex.ToString());
+            }
+            return response;
         }
 
-        public override Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
         {
-            return base.StreamingBothWays(requestStream, responseStream, context);
+            try
+            {
+                //When the request is cancled by client, an IOException will be thrown.
+                await foreach (var request in requestStream.ReadAllAsync(cancellationToken: context.CancellationToken))
+                {
+                    var response = new ExampleResponse() { Request = request };
+                    response.Titles.Add(GetTitles(request.PageIndex, request.PageSize, request.IsDescending));
+                    await responseStream.WriteAsync(response);
+                }
+            }
+            catch (System.IO.IOException ex)
+            {
+                _logger.LogWarning(ex.ToString());
+            }
         }
     }
 }
