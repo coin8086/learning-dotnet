9700014743162e1f6ab944ce80653f624baf6667
Robert Zhang <louirobert@gmail.com>
2022-03-11 12:34:57 +0800

    Add LoggingSample

---
+++ LoggingSample/FileLogger.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+
+namespace LoggingSample
+{
+    interface IFileLogWriter
+    {
+        void WriteMessage(string msg);
+    }
+
+    class FileLogger : ILogger
+    {
+        IFileLogWriter _logWriter;
+
+        string _name;
+
+        LogLevel _logLevel;
+
+        public FileLogger(IFileLogWriter logWriter, string name, LogLevel logLevel)
+        {
+            _logWriter = logWriter;
+            _name = name;
+            _logLevel = logLevel;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel >= _logLevel && logLevel != LogLevel.None;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (IsEnabled(logLevel))
+            {
+                var msg = $"[{DateTimeOffset.UtcNow.ToString("o")}][{_name}][{logLevel}][{eventId}]: {formatter(state, exception)}\n";
+                if (exception != null)
+                {
+                    msg += $"{exception}\n";
+                }
+                _logWriter.WriteMessage(msg);
+            }
+        }
+    }
+}
---
+++ LoggingSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("LoggingSample.Program", LogLevel.Information)
+                    .AddConsole()
+                    //NOTE: the cusomt file logger doesn't pick up log settings from appsettings.json.
+                    //And, its log level is only determined by the argument passed in here.
+                    .AddFileLogger("LogFile.txt", LogLevel.Error);
+            });
+
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogDebug("Example debug message");
+            logger.LogInformation("Example info message");
+            logger.LogWarning("Example warning message");
+            logger.LogError("Example error message");
+            logger.LogError(100, new ArgumentNullException(), "Example exception {msg}", "This is bad!");
+        }
+    }
+}
