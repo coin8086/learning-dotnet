46ec7bce6c693b6fd84049048c8388e3048c7f52
Robert Zhang <louirobert@gmail.com>
2022-05-06 11:27:41 +0800

    Add PathHash

---
+++ PathHash/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PathHash
+{
+    class Program
+    {
+        static (string, string) PathHash(string path)
+        {
+            var absPath = Path.GetFullPath(path);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                absPath = absPath.ToLower();
+            }
+            using var sha256 = SHA256.Create();
+            var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(absPath));
+            return (Convert.ToHexString(hash), absPath);
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                return 1;
+            }
+            var (hash, absPath) = PathHash(args[0]);
+            Console.WriteLine(absPath);
+            Console.WriteLine(hash);
+            return 0;
+        }
+    }
+}
