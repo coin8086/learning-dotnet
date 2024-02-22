07c3ca65ad9202800403d3e6c312c5cf1d84d385
Robert Zhang <louirobert@gmail.com>
2024-02-22 17:13:56 +0800

    Add Timers.

---
+++ Timers/SystemTimer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SystemTimer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var timer = new Timer(1000);
+        timer.Elapsed += TimerElapsed;
+        timer.Elapsed += TimerElapsedAsync;
+        timer.Start();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(false);
+    }
+
+    private static void TimerElapsed(object? sender, ElapsedEventArgs e)
+    {
+        Console.WriteLine($"Timer 1: {DateTimeOffset.Now}");
+    }
+
+    private static async void TimerElapsedAsync(object? sender, ElapsedEventArgs e)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine($"Timer 2: {DateTimeOffset.Now}");
+    }
+}
---
+++ Timers/ThreadTimer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTimer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var timer = new Timer(TimerCallback, null, 1000, 1000);
+        using var timer2 = new Timer(TimerCallbackAsync, null, 1000, 1000);
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(false);
+    }
+
+    static void TimerCallback(object _)
+    {
+        Console.WriteLine($"Timer 1: {DateTimeOffset.Now}");
+    }
+
+    static async void TimerCallbackAsync(object _)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine($"Timer 2: {DateTimeOffset.Now}");
+    }
+}
