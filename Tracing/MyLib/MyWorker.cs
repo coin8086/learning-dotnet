9030cf2bc6671cb54d94cbc1a4e3e39ab42e6c5e
Robert Zhang <louirobert@gmail.com>
2024-03-15 21:01:54 +0800

    Add Tracing/

---
+++ Tracing/MyLib/MyWorker.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace MyLib;
+
+public class MyWorker
+{
+    static ActivitySource _source = new ActivitySource(typeof(MyWorker).FullName!, "1.0.0");
+
+    public async Task DoSomeWork(string foo, int bar)
+    {
+        using (var activity = _source.StartActivity("SomeWork"))
+        {
+            activity?.SetTag("foo", foo);
+            activity?.SetTag("bar", bar);
+            await StepOne().ConfigureAwait(false);
+            activity?.AddEvent(new ActivityEvent("Step one done"));
+            await StepTwo().ConfigureAwait(false);
+            activity?.AddEvent(new ActivityEvent("Step two done"));
+        }
+    }
+
+    async Task StepOne()
+    {
+        using (var activity = _source.StartActivity("StepOne"))
+        {
+            await Task.Delay(500).ConfigureAwait(false);
+        }
+    }
+
+    async Task StepTwo()
+    {
+        using (var activity = _source.StartActivity("StepTwo"))
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Tracing/OpenTelemetryConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿//See https://learn.microsoft.com/en-us/dotnet/core/diagnostics/distributed-tracing-instrumentation-walkthroughs
+
+using MyLib;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+
+namespace OpenTelemetryConsole;
+
+class Program
+{
+    static ActivitySource _source = new ActivitySource(typeof(Program).FullName!, "1.0.0");
+
+    static async Task Main(string[] args)
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MyService"))
+            .AddSource(typeof(Program).FullName!)
+            .AddSource(typeof(MyWorker).FullName!)
+            .AddConsoleExporter()
+            .Build();
+
+        using (var activity = _source.StartActivity("Main"))
+        {
+            Console.WriteLine("Begin some work");
+
+            var worker = new MyWorker();
+            await worker.DoSomeWork("banana", 8).ConfigureAwait(false);
+
+            Console.WriteLine("Some work done");
+        }
+    }
+}
