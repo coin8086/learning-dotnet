3f76961a189c3d4bee83ea56fe7bd5a03aec2882
Robert Zhang <louirobert@gmail.com>
2024-03-28 17:37:18 +0800

    Add GRpc/GRpcHelloClient/

---
+++ GRpc/GRpcHelloClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+using GRpcHello;
+
+namespace GRpcHelloClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var channel = GrpcChannel.ForAddress(args[0]);
+        var client = new Greeter.GreeterClient(channel);
+        var result = client.SayHello(new HelloRequest() { Name = "Rob" });
+
+        Console.WriteLine(result.Message);
+    }
+}
