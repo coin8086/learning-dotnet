d91b846791d0810ddc2b4448871ed6797e030224
Robert Zhang <louirobert@gmail.com>
2024-05-09 11:49:14 +0800

    Add Class/InterfaceMethod

---
+++ Class/InterfaceMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace InterfaceMethod;
+
+interface IFoo
+{
+    //Not like a C++ interface method, the method Func is not "virtual".
+    void Func();
+}
+
+class Foo : IFoo
+{
+    //So here the "virtual" modifier must be specified when a subclass needs to override it.
+    public virtual void Func()
+    {
+        Console.WriteLine("Foo");
+    }
+}
+
+class Bar : Foo
+{
+    public override void Func()
+    {
+        Console.WriteLine("Bar");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var bar = new Bar();
+        bar.Func();
+    }
+}
