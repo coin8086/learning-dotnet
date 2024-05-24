63bf2d35a7f4a382e92bf32891fb140b4197d0f6
Robert Zhang <louirobert@gmail.com>
2024-05-24 18:03:07 +0800

    Add Task/SynchronizationContext

---
+++ Task/SynchronizationContext/Program.cs
@@ -0,0 +1,59 @@
+﻿//See https://devblogs.microsoft.com/dotnet/configureawait-faq/
+//and https://learn.microsoft.com/en-us/archive/msdn-magazine/2011/february/msdn-magazine-parallel-computing-it-s-all-about-the-synchronizationcontext
+
+namespace SynchronizationContextSample;
+
+class MySynchronizationContext : SynchronizationContext
+{
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        Console.WriteLine($"Post d={d}, state={state}");
+        Console.WriteLine(Environment.StackTrace);
+        base.Post(d, state);
+        //d(state);
+    }
+
+    public override void Send(SendOrPostCallback d, object? state)
+    {
+        Console.WriteLine($"Send d={d}, state={state}");
+        Console.WriteLine(Environment.StackTrace);
+        base.Send(d, state);
+        //d(state);
+    }
+
+    public override void OperationStarted()
+    {
+        Console.WriteLine("OperationStarted");
+        Console.WriteLine(Environment.StackTrace);
+        base.OperationStarted();
+    }
+
+    public override void OperationCompleted()
+    {
+        Console.WriteLine("OperationCompleted");
+        Console.WriteLine(Environment.StackTrace);
+        base.OperationCompleted();
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine(SynchronizationContext.Current);
+        SynchronizationContext.SetSynchronizationContext(new MySynchronizationContext());
+        Console.WriteLine(SynchronizationContext.Current);
+
+        Console.WriteLine($"Main starts at thread {Thread.CurrentThread.ManagedThreadId}.");
+
+        var task = Task.Run(() =>
+        {
+            Console.WriteLine($"I'm running at thread {Thread.CurrentThread.ManagedThreadId}.");
+        });
+
+        //NOTE: Compare the output of "await task" with task.Wait();
+        await task;
+
+        Console.WriteLine($"Main ends at thread {Thread.CurrentThread.ManagedThreadId}.");
+    }
+}
