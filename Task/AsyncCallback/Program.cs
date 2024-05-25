1dda0828d9956ec6e99273124076c7b587d0ca52
Robert Zhang <louirobert@gmail.com>
2024-05-25 13:29:24 +0800

    Add Task/AsyncCallback

---
+++ Task/AsyncCallback/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AsyncCallback;
+
+class Program
+{
+    static void CallCallback(Action callback)
+    {
+        Console.Error.WriteLine("CallCallback Begin");
+        callback();
+        Console.Error.WriteLine("CallCallback End");
+    }
+
+    static void WaitCallback(Func<Task> callback)
+    {
+        Console.Error.WriteLine("WaitCallback Begin");
+        callback().Wait();
+        Console.Error.WriteLine("WaitCallback End");
+    }
+
+    static void Main(string[] args)
+    {
+        Action cb = async () =>
+        {
+            Console.Error.WriteLine("Callback Begin");
+            await Task.Delay(2000);
+            Console.Error.WriteLine("Callback End");
+        };
+        CallCallback(cb);
+
+        Console.Error.WriteLine("-------------------------------");
+
+        Func<Task> cb2 = async () =>
+        {
+            Console.Error.WriteLine("Callback 2 Begin");
+            await Task.Delay(1000);
+            Console.Error.WriteLine("Callback 2 End");
+        };
+        WaitCallback(cb2);
+
+        Console.Error.WriteLine("-------------------------------");
+        Console.Error.WriteLine("Press any key to exit...");
+        Console.ReadKey(true);
+    }
+}
