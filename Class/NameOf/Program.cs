aa135e5195e0681f0c78e6e93e93cc74cff297df
Robert Zhang <louirobert@gmail.com>
2024-08-01 13:58:49 +0800

    Add projects NameOf and TypeOf

---
+++ Class/NameOf/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NameOf;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(nameof(Main));
+        Console.WriteLine(nameof(Program));
+        Console.WriteLine(nameof(NameOf));
+    }
+}
