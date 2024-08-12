01e5adb0b86e3977c48b517aef94949cdf4c151f
Robert Zhang <louirobert@gmail.com>
2024-08-12 17:29:20 +0800

    Add Test/TestInMSTest/TestInitAndCleanup.cs

---
+++ Test/TestInMSTest/TestInitAndCleanup.cs
@@ -0,0 +1,54 @@
+﻿namespace TestInMSTest;
+
+[TestClass]
+public class TestInitAndCleanup
+{
+    private static TestContext _testContext;
+
+    public TestInitAndCleanup()
+    {
+        _testContext.WriteLine("CTOR");
+    }
+
+    [ClassInitialize]
+    //Or: public static async Task TestInit(TestContext testContext)
+    public static void ClassInit(TestContext testContext)
+    {
+        _testContext = testContext;
+        _testContext.WriteLine("Test class init");
+    }
+
+    [ClassCleanup]
+    //Or: public static async Task TestCleanup()
+    public static void ClassCleanup()
+    {
+        _testContext.WriteLine("Test class cleanup");
+    }
+
+    [TestInitialize]
+    //Or: public async Task TestInit()
+    public void TestInit()
+    {
+        _testContext.WriteLine("Test init");
+    }
+
+    [TestCleanup]
+    //Or: public async Task TestCleanup()
+    public void TestCleanup()
+    {
+        _testContext.WriteLine("Test cleanup");
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true, "This is true.");
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Assert.IsTrue(true, "This is also true.");
+    }
+
+}
