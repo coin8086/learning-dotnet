6050dbc592ee164c096702ff28372fd674827ceb
Robert Zhang <louirobert@gmail.com>
2024-10-07 13:23:52 +0800

    Add RazorPagesMovie: step 2: Add a model to a Razor Pages app

--- HostedService/RazorPagesMovie/Program.cs
+++ HostedService/RazorPagesMovie/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
 namespace RazorPagesMovie;
 
 public class Program
@@ -5,6 +8,8 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddDbContext<MovieContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("MovieContext") ?? throw new InvalidOperationException("Connection string 'MovieContext' not found.")));
 
         // Add services to the container.
         builder.Services.AddRazorPages();
