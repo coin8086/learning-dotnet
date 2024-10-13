5edfff6a9004527e23c0cab1415006d40f0098f8
Robert Zhang <louirobert@gmail.com>
2024-10-13 11:31:50 +0800

    Add RazorPagesMovie: step 4: Seed the database

--- HostedService/RazorPagesMovie/Program.cs
+++ HostedService/RazorPagesMovie/Program.cs
@@ -18,6 +18,13 @@ public class Program
 
         var app = builder.Build();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            SeedData.Initialize(services);
+        }
+
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
