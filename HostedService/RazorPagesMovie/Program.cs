8573e2a535804f6caad24d23348d92d3d4579c9e
Robert Zhang <louirobert@gmail.com>
2024-10-13 11:01:42 +0800

    Add RazorPagesMovie: step 3: Scaffolded Razor Pages

--- HostedService/RazorPagesMovie/Program.cs
+++ HostedService/RazorPagesMovie/Program.cs
@@ -1,3 +1,5 @@
+//See https://learn.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/?view=aspnetcore-8.0
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Data;
