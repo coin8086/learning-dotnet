1a784b88bb3ad5ae88deb9608dbd435ea1c4af51
Robert Zhang <louirobert@gmail.com>
2024-10-26 21:09:34 +0800

    Add Logging/LoggingSrcGen

---
+++ Logging/LoggingSrcGen/Program.cs
@@ -0,0 +1,30 @@
+﻿//See https://learn.microsoft.com/en-us/dotnet/core/extensions/logger-message-generator
+
+using Microsoft.Extensions.Logging;
+
+namespace LoggingSrcGen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddSimpleConsole();
+        });
+
+        ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+        logger.Log(LogLevel.Information, 100, "Bob", "Hello");
+        logger.LogWarning(100, "Bob", "Hello");
+    }
+}
+
+public static partial class CustomLog
+{
+    [LoggerMessage("[{id}, {name}]: {msg}")]
+    public static partial void Log(this ILogger logger, LogLevel level, int id, string name, string msg);
+
+
+    [LoggerMessage(LogLevel.Warning, "[{id}, {name}]: {msg}")]
+    public static partial void LogWarning(this ILogger logger, int id, string name, string msg);
+}
