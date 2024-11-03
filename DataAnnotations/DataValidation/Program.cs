ce9c5c616b5a31a79642beab1e78cf29105c8e9a
Robert Zhang <louirobert@gmail.com>
2024-11-03 21:00:45 +0800

    Add DataAnnotations/DataValidation

---
+++ DataAnnotations/DataValidation/Program.cs
@@ -0,0 +1,53 @@
+﻿//See https://weblogs.asp.net/ricardoperes/net-8-data-annotations-validation
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DataValidation;
+
+class Program
+{
+    static void TryValidateUser(User user)
+    {
+        //The variable results as parameter to TryValidateObject is optional. It can be null.
+        var results = new List<ValidationResult>();
+        if (!Validator.TryValidateObject(user, new ValidationContext(user), results, true))
+        {
+            Console.WriteLine($"Invalid {user}");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Valid {user}");
+        }
+    }
+
+    static void ValidateUser(User user)
+    {
+        try
+        {
+            Validator.ValidateObject(user, new ValidationContext(user));
+            Console.WriteLine($"Valid {user}");
+        }
+        catch (ValidationException ex)
+        {
+            Console.WriteLine($"Invalid {user}");
+            Console.WriteLine(ex.ValidationResult);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var user = new User()
+        {
+            Email = "xxx"
+        };
+        TryValidateUser(user);
+
+        Console.WriteLine("-----------------");
+
+        ValidateUser(user);
+    }
+}
---
+++ DataAnnotations/DataValidation/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace DataValidation;
+
+class User
+{
+    [Required]
+    public string? Name { get; set; }
+
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    public override string ToString()
+    {
+        var opts = new JsonSerializerOptions() { WriteIndented = true };
+        var json = JsonSerializer.Serialize(this, opts);
+        return json;
+    }
+}
