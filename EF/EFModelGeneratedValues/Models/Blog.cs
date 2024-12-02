aaff8796c266b6da866f9aec99dfcaa0099ff268
Robert Zhang <louirobert@gmail.com>
2024-12-02 12:28:45 +0800

    Add EFModelGeneratedValues

---
+++ EF/EFModelGeneratedValues/Models/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
+namespace EFModelGeneratedValues.Models;
+
+public class Blog
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; }
+
+    //NOTE: The there's a bug on the value "Computed" for the SQLite provider.
+    //Change it to "Identity" for a workaround.
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedAt { get; set; }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
---
+++ EF/EFModelGeneratedValues/Program.cs
@@ -0,0 +1,29 @@
+﻿//See https://learn.microsoft.com/en-us/ef/core/modeling/generated-properties?tabs=data-annotations
+
+using EFModelGeneratedValues.Data;
+using EFModelGeneratedValues.Models;
+
+namespace EFModelGeneratedValues;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var db = new DataContext();
+        Console.WriteLine($"DB file: {db.DbFilePath}");
+
+        //db.Database.EnsureDeleted();
+        //db.Database.EnsureCreated();
+
+        var ts = DateTime.Now;
+        //NOTE: For the SQLite provider, CreatedAt and UpdatedAt have to be set manually.
+        var blog = new Blog() { Name = "Blog 1", CreatedAt = ts, UpdatedAt = ts };
+        var blogEntity = db.Add(blog);
+        Console.WriteLine(blogEntity);
+
+        db.SaveChanges();
+
+        Console.WriteLine("----------------");
+        Console.WriteLine(blogEntity);
+    }
+}
