6264f40421387221b589ba86456e0f8c1f58fcd0
Robert Zhang <louirobert@gmail.com>
2025-05-14 17:21:22 +0800

    HostedService/Worker: Show how to exit the hosted application.

--- HostedService/Worker/Worker.cs
+++ HostedService/Worker/Worker.cs
@@ -1,23 +1,30 @@
+
 namespace Worker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IHostApplicationLifetime _lifetime;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IHostApplicationLifetime lifetime)
     {
         _logger = logger;
+        _lifetime = lifetime;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        var count = 0;
+        while (!stoppingToken.IsCancellationRequested && count < 10)
         {
+            count++;
             if (_logger.IsEnabled(LogLevel.Information))
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
             await Task.Delay(1000, stoppingToken);
         }
+        _logger.LogInformation("Worker is over at: {time}", DateTimeOffset.Now);
+        _lifetime.StopApplication();
     }
 }
